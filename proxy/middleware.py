a04093321393a2f879d15b677ccc03aeaf933b51
Alex Sandro <geniofuturo@gmail.com>
2014-10-19 20:31:12 -0200

    Corrigindo bugs na implementação do cache no carregamento de imagens.

--- proxy/middleware.py
+++ proxy/middleware.py
@@ -1,34 +1,69 @@
+import StringIO
+import copy
 from django.http import HttpResponse, StreamingHttpResponse
 from django.core.cache import get_cache, DEFAULT_CACHE_ALIAS
 import requests
 from proxy import utils
+import time
+import hashlib
 
 __author__ = 'alex'
 
 
+class Iterator(object):
+
+    def __init__(self, data):
+        self.data = data
+
+    def __iter__(self):
+        counter = 1024
+        while True:
+            before = time.time()
+            chunk = self.data.read(counter)
+            print chunk
+            if not chunk:
+                break
+            after = time.time()
+            counter = self.best_block_size((after-before), chunk)
+            yield chunk
+        raise StopIteration
+
+    @staticmethod
+    def best_block_size(elapsed_time, bytes):
+        new_min = max(bytes / 2.0, 1.0)
+        new_max = min(max(bytes * 2.0, 1.0), 4194304) # Do not surpass 4 MB
+        if elapsed_time < 0.001:
+            return long(new_max)
+        rate = bytes / elapsed_time
+        if rate > new_max:
+            return long(new_max)
+        if rate < new_min:
+            return long(new_min)
+        return long(rate)
+
+
 class Cache(object):
-    sep = '_'
+    sep = ':'
 
     def __init__(self, scope):
-        self.scope = scope
+        self.scope = hashlib.md5(scope).hexdigest()
         self.cache = get_cache(DEFAULT_CACHE_ALIAS)
 
+    def join(self, name):
+        return self.sep.join([self.scope, name])
+
     def __getitem__(self, key):
-        return self.cache.get(self.sep.join([self.scope, key]))
+        return self.cache.get(self.join(key))
 
     def __setitem__(self, key, value):
-        self.cache.add(self.sep.join([self.scope, key]), value)
+        self.cache.add(self.join(key), value)
 
     def has(self, name):
-        return self.cache.has_key(self.sep.join([self.scope, name]))
+        return self.cache.has_key(self.join(name))
 
     def iter(self, name):
-        text = self.cache.get(self.sep.join([self.scope, name]))
-        counter = 2048
-        while text:
-            yield text[:counter]
-            text = text[counter:]
-        raise StopIteration
+        content = self.cache.get(self.join(name))
+        return Iterator(StringIO.StringIO(content))
 
 
 class ProxyRequest(object):
@@ -51,49 +86,69 @@ class ProxyRequest(object):
     ]
 
     def __init__(self):
-        self.request = self.cache = None
+        pass
 
-    @property
-    def path(self):
-        path = self.request.path.lstrip('/').strip()
+    @staticmethod
+    def get_path(request):
+        path = request.path.lstrip('/').strip()
         if not path or not path.startswith('http'):
-            path = utils.get_request_absolute_url(self.request)
+            path = utils.get_request_absolute_url(request)
         return path
 
     def process_request(self, request):
-        self.request = request
-        self.cache = Cache(self.path)
+        cache = Cache(self.get_path(request))
 
-        if self.cache.has(self.CONTENT) and self.cache.has(self.HEADERS):
-            response = self._response_cache()
+        if cache.has(self.CONTENT) and cache.has(self.HEADERS):
+            response = self._response_cache(request, cache)
         else:
-            response = self._response_web()
+            response = self._response_web(request, cache)
 
         return response
 
-    def _response_cache(self):
-        response = StreamingHttpResponse(self.cache.iter(self.CONTENT))
-        for header, value in self.cache[self.HEADERS].iteritems():
+    def _response_cache(self, request, cache):
+        stream = False
+        for value in cache[self.HEADERS].itervalues():
+            if value.startswith('image'):
+                stream = True
+                break
+        if stream:
+            response = StreamingHttpResponse(cache.iter(self.CONTENT))
+        else:
+            response = HttpResponse(cache[self.CONTENT])
+        for header, value in cache[self.HEADERS].iteritems():
             response[header] = value
         return response
 
-    def _response_web(self):
+    def _response_web(self, request, cache):
         session = requests.Session()
         session.trust_env = False
 
-        req = session.request(self.request.method, self.path, proxies=self.NO_PROXY)
+        headers = copy.deepcopy(request.GET)
+        headers.update(request.POST)
+
+        req = session.request(request.method, self.get_path(request), proxies=self.NO_PROXY, headers=headers)
+
+        if req.headers['content-type'].startswith('image'):
+            req = session.request(request.method, self.get_path(request), proxies=self.NO_PROXY,
+                                  stream=True)
+            response = StreamingHttpResponse(req.raw)
+            self.copy_headers_to(req.headers, response)
+            return response
+
         text = req.text
 
         response = HttpResponse(text)
+        headers = self.copy_headers_to(req.headers, response)
 
-        headers = {}
-
-        for header, value in req.headers.iteritems():
-            if not header.lower() in self.HOP_BY_HOP_HEADER:
-                response[header] = value
-                headers[header] = value
-
-        self.cache[self.HEADERS] = headers
-        self.cache[self.CONTENT] = text
+        cache[self.HEADERS] = headers
+        cache[self.CONTENT] = text
         return response
 
+    @classmethod
+    def copy_headers_to(cls, headers, response):
+        _headers = {}
+        for header, value in headers.iteritems():
+            if not header.lower() in cls.HOP_BY_HOP_HEADER:
+                response[header] = value
+                _headers[header] = value
+        return _headers
